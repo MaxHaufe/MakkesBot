a89f39375ac3a38d4da29f0cb7ecc23882e94f3e
MaxHaufe <haufemax@gmail.com>
2021-03-31 11:51:15 +0200

    update

--- README.md
+++ README.md
@@ -1,16 +1,8 @@
 # MakkesBot
 
-python3 main.py $path_to_chrome_driver
+python3 main.py 
 
 
 get python
 https://www.python.org/downloads/release/python-390/
-
-get seleniun
-
-```$ python -m pip install selenium```
-
-get google chrome - get version
-
-get chrome driver (version, OS)
 
--- main.py
+++ main.py
@@ -1,42 +1,38 @@
 import sys
 import time
 import sqlite3
-
+from joblib import Parallel, delayed
 
 import rl_garage
 import rl_insider
 
 from datetime import datetime
 
-
 # TODO timeout and other exceptions
-
-path_to_chrome_driver = sys.argv[1]
 
 connection = sqlite3.connect("database.db")
 start_time = time.time()
 
-url = "https://rocket-league.com/trading?filterItem=0&filterCertification=0&filterPaint=0&filterMinCredits=0" \
-      "&filterMaxCredits=100000&filterPlatform%5B%5D=1&filterSearchType=1&filterItemType=0 "
+# url = "https://rocket-league.com/trading?filterItem=0&filterCertification=0&filterPaint=0&filterMinCredits=0&filterMaxCredits=100000&filterPlatform%5B%5D=1&filterSearchType=1&filterItemType=0"
+url_flags = "?filterItem=0&filterCertification=0&filterPaint=0&filterMinCredits=1500&filterMaxCredits=3000&filterPlatform%5B%5D=1&filterSearchType=1&filterItemType=0"
+
+# url = "https://rocket-league.com/trading?filterItem=0&filterCertification=0&filterPaint=0&filterMinCredits=0" \
+#       "&filterMaxCredits=100000&filterPlatform%5B%5D=1&filterSearchType=1&filterItemType=0 "
 # url = "https://rocket-league.com/trades/DerMa_2007"
 
 cursor = connection.cursor()
 cursor.execute('PRAGMA FOREIGN_KEYS = ON')
-# not a lot of results
-# url = "https://rocket-league.com/trading?filterItem=1694&filterCertification=0&filterPaint=0&filterMinCredits=0&filterMaxCredits=100000&filterSearchType=1&filterItemType=0"
 
-# prints all trades of n pages TODO integrate in class
-# time should be defined prior to calling loag_n_pages because it takes forever
 # cursor.execute('DELETE FROM rl_garage_trade')
 # connection.commit()
-for i in range(50):
-    print("Loop Nr: " + str(i + 1))
-    time_now = datetime.now()
-    trade_list = rl_garage.eval_url_bs4(rl_garage.load_n_pages(url, 10, path_to_chrome_driver), time_now)
-    rl_garage.insert_trade_list_into_database(connection, trade_list)
+start_time = time.time()
 
+rl_garage.parse_n_pages(url_flags, 10000, connection)
+# trade_list = rl_garage.eval_url_bs4(
+#     rl_garage.parse_n_pages(url_flags, 1), time_now)
+# rl_garage.insert_trade_list_into_database(connection, trade_list)
+# print("--- %s seconds ---" % (time.time() - start_time))
 
-# scans rl.insider.gg for ALL item prices
 # rl_insider_url = "https://rl.insider.gg/en/pc?mobile" # chrome
 rl_insider_url = "https://rl.insider.gg/en/pc"  # firefox only wtf??
 
@@ -46,4 +42,4 @@ rl_insider_url = "https://rl.insider.gg/en/pc"  # firefox only wtf??
 # rl_insider.insert_all_into_database(connection, rl_insider_url)
 
 connection.close()
-print("--- %s seconds ---" % (time.time() - start_time))
+# print("--- %s seconds ---" % (time.time() - start_time))
